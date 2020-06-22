5f5c0fc0c3706862feb7a41aaf5546689dcc5cc1
김케클 <keinclonicle@gmail.com>
2020-06-22 18:25:46 +0900

    오류....

--- src/App.js
+++ src/App.js
@@ -13,14 +13,6 @@ import ContactUs from './components/contactUs'
 
 import Footer from './components/footer'
 
-import { createStore } from 'redux';
-import { useDispatch } from 'react-redux';
-import { Provider } from 'react-redux';
-// store root
-import rootReducer from './store'
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
 // Hook
 function useWindowSize() {
   const isClient = typeof window === 'object';
@@ -52,26 +44,24 @@ function useWindowSize() {
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App" style={{wordBreak:'keep-all'}}>
-        <div className="container-md menuWrap fixed-top">
-          <Menu />
-        </div>
-        <div style={{marginTop: useWindowSize().width < 500 ? "198px" : "110px"}} className="container-fluid sliderWrap">
-          <Slider />
-        </div>
-        <div className="contentWrap">
-          <Intro />
-          <Content1 />
-          <Content2 />
-          <Content3 />
-          <ContactUs />
-        </div>
-        <div className="container-fluid footerWrap">
-          <Footer />
-        </div>
+    <div className="App" style={{wordBreak:'keep-all'}}>
+      <div className="container-md menuWrap fixed-top">
+        <Menu />
       </div>
-    </Provider>
+      <div style={{marginTop: useWindowSize().width < 500 ? "198px" : "110px"}} className="container-fluid sliderWrap">
+        <Slider />
+      </div>
+      <div className="contentWrap">
+        <Intro />
+        <Content1 />
+        <Content2 />
+        <Content3 />
+        <ContactUs />
+      </div>
+      <div className="container-fluid footerWrap">
+        <Footer />
+      </div>
+    </div>
   );
 }
 
